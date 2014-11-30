4ed29a07f436193490238fd78ec911b01634147e
Ivan Samborskiy <whiplash1993@yandex.ru>
2014-11-30 14:42:37 +0300

    add TwitterHelper

--- DataExtraction/src/main/java/com/samborskiy/extraction/Main.java
+++ DataExtraction/src/main/java/com/samborskiy/extraction/Main.java
@@ -1,13 +1,40 @@
 // CHECKSTYLE:OFF
 package com.samborskiy.extraction;
 
+import com.samborskiy.extraction.utils.DatabaseHelper;
+import com.samborskiy.extraction.utils.TwitterHelper;
+import twitter4j.TwitterException;
+import twitter4j.User;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TwitterException {
         File file = new File("DataExtraction/res/ru/config.json");
         Configuration configuration = Configuration.build(file);
+
+        TwitterHelper twitterHelper = new TwitterHelper(configuration);
+        List<User> users = new ArrayList<>();
+        List<String> usersScreenName = new ArrayList<>();
+        List<String> tweets = new ArrayList<>();
+        users.add(twitterHelper.getUser(configuration.getInitPersonalScreenName()));
+        users.addAll(twitterHelper.getFollowers(users.get(0)));
+        for (User user : users) {
+            System.out.println(user.getScreenName());
+            usersScreenName.add(user.getScreenName());
+            tweets.add(twitterHelper.getTweets(user.getScreenName()));
+        }
+
+        try (DatabaseHelper dbHelper = new DatabaseHelper(configuration)) {
+            dbHelper.createTable();
+            dbHelper.insertAll(usersScreenName, tweets, 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
---
+++ DataExtraction/src/main/java/com/samborskiy/extraction/utils/TwitterHelper.java
@@ -0,0 +1,147 @@
+package com.samborskiy.extraction.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.samborskiy.extraction.Configuration;
+import twitter4j.PagableResponseList;
+import twitter4j.Paging;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class which help user interact with twitter API.
+ *
+ * @author Whiplash
+ */
+public class TwitterHelper {
+
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    static {
+        mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+    }
+
+    private Configuration configuration;
+    private Twitter twitter;
+
+    /**
+     * Creates new instance of {@code TwitterHelper} with
+     * initialize {@code configuration}.
+     *
+     * @param configuration needed to connect to database and get
+     *                      some parameters to data extraction
+     */
+    public TwitterHelper(Configuration configuration) {
+        this.configuration = configuration;
+        twitter = new TwitterFactory().getInstance();
+    }
+
+    /**
+     * Returns information about user by screen name.
+     *
+     * @param screenName screen name of user
+     * @return information about user
+     */
+    public User getUser(String screenName) {
+        try {
+            User user = twitter.showUser(screenName);
+            if (isCorrectUser(user)) {
+                return user;
+            }
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Returns user tweets in json format.
+     *
+     * @param screenName screen name of user
+     * @return user tweets
+     */
+    public String getTweets(String screenName) {
+        try {
+            Paging paging = new Paging(1, configuration.getTweetPerUser());
+            List<Status> statuses = twitter.getUserTimeline(screenName, paging);
+            StringWriter str = new StringWriter();
+            mapper.writeValue(str, filterTweets(statuses));
+            return str.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Returns filtered tweets of user.
+     *
+     * @param statuses user statuses (tweets)
+     * @return user tweets
+     */
+    private List<String> filterTweets(List<Status> statuses) {
+        List<String> tweets = new ArrayList<>();
+        for (Status status : statuses) {
+            if (!status.isRetweet() && status.getLang().equals(configuration.getLang())) {
+                tweets.add(status.getText());
+            }
+        }
+        return tweets;
+    }
+
+    /**
+     * Returns all followers of user.
+     *
+     * @param user user from whom you want to request followers
+     * @return user's followers
+     */
+    public List<User> getFollowers(User user) {
+        long cursor = -1;
+        PagableResponseList<User> followers;
+        List<User> users = new ArrayList<>();
+        try {
+            do {
+                followers = twitter.getFollowersList(user.getId(), cursor);
+                users.addAll(filterFollowers(followers));
+            } while ((cursor = followers.getNextCursor()) != 0);
+            return users;
+        } catch (TwitterException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Returns list of filtered users.
+     *
+     * @param followers followers of user
+     * @return list of followers
+     */
+    private List<User> filterFollowers(PagableResponseList<User> followers) {
+        List<User> users = new ArrayList<>();
+        for (User follower : followers) {
+            if (isCorrectUser(follower)) {
+                users.add(follower);
+            }
+        }
+        return users;
+    }
+
+    /**
+     * Checks is correct user or not.
+     *
+     * @param user user whom will be checked
+     * @return {@code true} if user is correct
+     */
+    private boolean isCorrectUser(User user) {
+        return user.getLang().equals(configuration.getLang()) && !user.isProtected();
+    }
+
+}
