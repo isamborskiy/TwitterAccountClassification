daaa1dac893fe73ec54d19e872245c6d1937c151
Ivan Samborskiy <whiplash1993@yandex.ru>
2015-03-30 22:54:56 +0300

    Added cross validation test 2.

---
+++ Algorithm/src/main/java/com/samborskiy/statistic/Statistics.java
@@ -0,0 +1,113 @@
+package com.samborskiy.statistic;
+
+/**
+ * Structure for holding statistics of test.
+ * <p>
+ * Created by warrior on 19.09.14.
+ */
+public class Statistics {
+
+    private double fMeasure;
+    private double precision;
+    private double recall;
+
+    /**
+     * Creates instance of {@code Statistics} with init precision and recall.
+     *
+     * @param precision {@link #precision}
+     * @param recall    {@link #recall}
+     */
+    public Statistics(double precision, double recall) {
+        this.precision = precision;
+        this.recall = recall;
+        this.fMeasure = computeFDistance(precision, recall);
+    }
+
+    /**
+     * Computes F-Measure value
+     *
+     * @param precision {@link #precision}
+     * @param recall    {@link #recall}
+     * @return F-Measure value
+     */
+    private static double computeFDistance(double precision, double recall) {
+        return 2 * precision * recall / (precision + recall);
+    }
+
+    public double getFMeasure() {
+        return fMeasure;
+    }
+
+    public double getPrecision() {
+        return precision;
+    }
+
+    public double getRecall() {
+        return recall;
+    }
+
+    /**
+     * Gets {@link #Statistics} using {@code confusionMatrix}
+     *
+     * @param confusionMatrix confusion matrix
+     * @return {@link #Statistics}
+     */
+    public static Statistics createStatistics(int[][] confusionMatrix) {
+        checkConfusionMatrix(confusionMatrix);
+        double[] answer = new double[2];
+        computePrecisionAndRecall(confusionMatrix, answer);
+        return new Statistics(answer[0], answer[1]);
+    }
+
+    private static void checkConfusionMatrix(int[][] confusionMatrix) {
+        if (confusionMatrix == null) {
+            throw new IllegalArgumentException("confusionMatrix must be not null");
+        }
+        if (confusionMatrix.length != confusionMatrix[0].length) {
+            throw new IllegalArgumentException("confusionMatrix must be square");
+        }
+    }
+
+    /**
+     * @param ans answer array. After method's execution {@code ans[0]} contains precision,
+     *            {@code ans[1]} contains recall
+     * @see <a href="http://bazhenov.me/blog/2012/07/21/classification-performance-evaluation.html">confusionMatrix</a>
+     */
+    private static void computePrecisionAndRecall(int[][] confusionMatrix, double[] ans) {
+        if (confusionMatrix == null) {
+            throw new IllegalArgumentException("confusionMatrix must be not null");
+        }
+        if (confusionMatrix.length != confusionMatrix[0].length) {
+            throw new IllegalArgumentException("confusionMatrix must be square");
+        }
+        if (ans == null || ans.length < 2) {
+            throw new IllegalArgumentException("answer array length must be >= 2");
+        }
+        int size = confusionMatrix.length;
+        int precisionCount = 0;
+        int recallCount = 0;
+        ans[0] = 0;
+        ans[1] = 0;
+        for (int i = 0; i < size; i++) {
+            double sum = 0;
+            for (int j = 0; j < size; j++) {
+                sum += confusionMatrix[i][j];
+            }
+            if (sum != 0) {
+                ans[0] += confusionMatrix[i][i] / sum;
+                precisionCount++;
+            }
+            sum = 0;
+            for (int j = 0; j < size; j++) {
+                sum += confusionMatrix[j][i];
+            }
+            if (sum != 0) {
+                ans[1] += confusionMatrix[i][i] / sum;
+                recallCount++;
+            }
+
+        }
+        ans[0] /= precisionCount;
+        ans[1] /= recallCount;
+    }
+}
---
+++ Algorithm/src/main/java/com/samborskiy/statistic/TestMachine.java
@@ -0,0 +1,103 @@
+package com.samborskiy.statistic;
+
+import com.samborskiy.algorithm.Classifier;
+import com.samborskiy.entity.Instance;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Whiplash on 30.03.2015.
+ */
+public class TestMachine<E extends Instance> {
+
+    private final Classifier<E> classifier;
+    private final List<E> data;
+    private final int size;
+    private final int[][] testConfusionMatrix;
+
+    public TestMachine(Classifier<E> classifier, List<E> data, int classNumber) {
+        if (data == null || data.isEmpty()) {
+            throw new IllegalArgumentException("data must be not empty");
+        }
+        this.classifier = classifier;
+        this.data = data;
+        this.size = data.size();
+        this.testConfusionMatrix = new int[classNumber][classNumber];
+    }
+
+    public Statistics crossValidationTest(int foldCount, int rounds, boolean parallelTest) {
+        int foldSize = size / foldCount;
+        for (int k = 0; k < rounds; k++) {
+            Collections.shuffle(data);
+            List<E> trainingData = new ArrayList<>(size - foldSize);
+            List<E> testData;
+            for (int i = 0; i < foldCount; i++) {
+                trainingData.clear();
+                int l = i * foldSize;
+                int r = l + foldSize;
+                testData = data.subList(l, r);
+                trainingData.addAll(data.subList(0, l));
+                trainingData.addAll(data.subList(r, size));
+                classifier.clear();
+                classifier.train(trainingData);
+                testInternal(trainingData, testData, parallelTest);
+            }
+        }
+        return getCurrentStatistic();
+    }
+
+    private Statistics testInternal(List<E> trainingData, List<E> testData, boolean parallelTest) {
+        clearConfusionMatrix();
+        if (parallelTest) {
+            testInternal(classifier, testData, testConfusionMatrix);
+        } else {
+            parallelTestInternal(classifier, testData, testConfusionMatrix);
+        }
+        return getCurrentStatistic();
+    }
+
+    public Statistics getCurrentStatistic() {
+        return Statistics.createStatistics(testConfusionMatrix);
+    }
+
+    private void testInternal(Classifier<E> classifier, List<E> data, int[][] confusionMatrix) {
+        for (E elem : data) {
+            confusionMatrix[classifier.getClassId(elem)][elem.getClassId()]++;
+        }
+    }
+
+    private void parallelTestInternal(Classifier<E> classifier, List<E> data, int[][] confusionMatrix) {
+        ExecutorService executor = generateExecutorService();
+        List<Future<Integer>> futureList = new ArrayList<>(data.size());
+        for (E elem : data) {
+            futureList.add(executor.submit(() -> classifier.getClassId(elem)));
+        }
+        try {
+            executor.shutdown();
+            executor.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
+            for (int i = 0; i < data.size(); i++) {
+                confusionMatrix[futureList.get(i).get()][data.get(i).getClassId()]++;
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ExecutorService generateExecutorService() {
+        return Executors.newFixedThreadPool(3);
+    }
+
+    private void clearConfusionMatrix() {
+        for (int[] arr : testConfusionMatrix) {
+            Arrays.fill(arr, 0);
+        }
+    }
+}
